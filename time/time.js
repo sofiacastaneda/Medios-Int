edb78e18a7201f66f3700288edff0f2eee42441a
sofiacastaneda <47247552+sofiacastaneda@users.noreply.github.com>
2019-03-12 13:23:30 -0500

    Add files via upload

--- time/time.js
+++ time/time.js
@@ -16,10 +16,10 @@ var expMinuY = 0;
 var expHoraX = 0;
 var expHoraY = 0;
 
-var f=0;
-var g=0;
-var h=0;
-var i=0;
+var f = 0;
+var g = 0;
+var h = 0;
+var i = 0;
 
 
 function setup() {
@@ -40,7 +40,7 @@ function draw() {
   hora2 = map(hora, 0, 23, 0, width);
 
 
-  
+
   //Barra 1: segundero
   strokeWeight(4);
 
@@ -55,7 +55,7 @@ function draw() {
   strokeWeight(6);
 
   noStroke();
-  fill(98,152,253);
+  fill(98, 152, 253);
   rect(10, 0, 20, height);
   stroke(0);
 
@@ -64,7 +64,7 @@ function draw() {
   strokeWeight(8);
 
   noStroke();
-  fill(255,255,255);
+  fill(255, 255, 255);
   rect(0, 360, width, 30);
   stroke(0);
   //line(hora2, 100, hora2, 130);
@@ -75,19 +75,19 @@ function draw() {
   //Círculo 1: segundero
   stroke(255, 197, 18);
   strokeWeight(4);
-    fill(255, 197, 18);
+  fill(255, 197, 18);
   ellipse(seg2, seg2, 10, 10);
 
   //Círculo 2: minutero
-  stroke(98,152,253);
+  stroke(98, 152, 253);
   strokeWeight(6);
-    fill(98,152,253);
+  fill(98, 152, 253);
   ellipse(minu2, minu2, 20, 20);
 
   //Círculo 3: hora
-  stroke(255,255,255);
+  stroke(255, 255, 255);
   strokeWeight(10);
-    fill(255,255,255);
+  fill(255, 255, 255);
   ellipse(hora2, 320, 30, 30);
 
   // Línea que se alarga con círculo 1;; segundero
@@ -96,18 +96,18 @@ function draw() {
   line(seg2, 20, seg2, seg2);
 
   // Línea que se alarga con círculo 2;; minutero
-  stroke(98,152,253);
+  stroke(98, 152, 253);
   strokeWeight(6);
   line(10, minu2, minu2, minu2);
 
   // Línea que sigue al círculo 3;; hora
-  stroke(255,255,255);
+  stroke(255, 255, 255);
   strokeWeight(8);
   line(hora2, 320, hora2, 390);
 
-  
 
-  
+
+
   /*
   //torso
     line(mouseX,mouseY,mouseX,mouseY+35);
@@ -121,169 +121,190 @@ function draw() {
     line(mouseX,mouseY+35,mouseX+10,mouseY+55);
 */
 
-  
 
-  
+
+
   //generador de círculos random segundero
 
   if (dist(mouseX, mouseY, seg2, seg2) < 20) {
     stroke(255, 197, 18);
     strokeWeight(4);
-      fill(255, 197, 18);
+    fill(255, 197, 18);
     ellipse(expSegX, expSegY, 10, 10);
     expSegX = random(0, width);
     expSegY = random(0, height);
 
     for (var f = 0; f < 100; f++) {
-            fill(255, 197, 18);
+      fill(255, 197, 18);
       ellipse(random(0, width), random(0, height), 10, 10);
     }
 
+    /*
+        //generador de círculos random minutero
 
-    //generador de círculos random minutero
+        if (dist(mouseX, mouseY, minu2, minu2) < 30) {
+          stroke(98,152,253);
+          fill(98,152,253);
+          strokeWeight(4);
+          ellipse(expMinuX, expMinuY, 20, 20);
+          expMinuX = random(0, width);
+          expMinuY = random(0, height);
 
-    if (dist(mouseX, mouseY, minu2, minu2) < 30) {
-      stroke(98,152,253);
-      fill(98,152,253);
-      strokeWeight(4);
-
-      ellipse(expMinuX, expMinuY, 20, 20);
-      expMinuX = random(0, width);
-      expMinuY = random(0, height);
-
-      for (var g = 0; g < 150; g++) {
-              fill(98,152,253);
-        ellipse(random(0, width), random(0, height), 20, 20);
-      }
-    }
-
-
-    //generador de círculos random indhora
-    if (dist(mouseX, mouseY, hora2, hora2) < 40) {
-      stroke(255,255,255);
-      strokeWeight(4);
-      ellipse(expHoraX, expHoraY, 10, 10);
-      expHoraX = random(0, width);
-      expHoraY = random(0, height);
-
-      for (var h = 0; h < 250; h++) {
-        fill(255,255,255);
-        ellipse(random(0, width), random(0, height), 35, 35);
-      }
-
-    }
-
-/*
-    //generador random por proximidad entre segundero y minutero
-    if (dist(seg2, seg2, minu2, minu2) < 20) {
-      stroke(255, 255, 0);
-      strokeWeight(4);
-      ellipse(x, y, 10, 10);
-      x = random(0, width);
-      y = random(0, height);
-
-      for (var i = 0; i < 20; i++) {
-        ellipse(random(0, width), random(0, height), 10, 10);
-      }
+          for (var g = 0; g < 150; g++) {
+                  fill(98,152,253);
+            ellipse(random(0, width), random(0, height), 20, 20);
+          }
+        }
 
 
-      //generador random por proximidad entre segundero y hora
-    if (dist(seg2, seg2, hora2, hora2) < 20) {
-      stroke(255, 255, 0);
-      strokeWeight(4);
-      ellipse(x, y, 10, 10);
-      x = random(0, width);
-      y = random(0, height);
+        //generador de círculos random indhora
+        if (dist(mouseX, mouseY, hora2, 320) < 40) {
+          stroke(255,255,255);
+          strokeWeight(4);
+          ellipse(expHoraX, expHoraY, 10, 10);
+          expHoraX = random(0, width);
+          expHoraY = random(0, height);
+          for (var h = 0; h < 250; h++) {
+            fill(255,255,255);
+            ellipse(random(0, width), random(0, height), 35, 35);
+          }
 
-      for (var i = 0; i < 20; i++) {
-        ellipse(random(0, width), random(0, height), 10, 10);
-      }
-      
-      }
-    }
-*/
-    
-    
-    
+        }
+
+
+        //generador random por proximidad entre segundero y minutero
+        if (dist(seg2, seg2, minu2, minu2) < 20) {
+          stroke(255, 255, 0);
+          strokeWeight(4);
+          ellipse(x, y, 10, 10);
+          x = random(0, width);
+          y = random(0, height);
+
+          for (var i = 0; i < 20; i++) {
+            ellipse(random(0, width), random(0, height), 10, 10);
+          }
+
+
+          //generador random por proximidad entre segundero y hora
+        if (dist(seg2, seg2, hora2, hora2) < 20) {
+          stroke(255, 255, 0);
+          strokeWeight(4);
+          ellipse(x, y, 10, 10);
+          x = random(0, width);
+          y = random(0, height);
+
+          for (var i = 0; i < 20; i++) {
+            ellipse(random(0, width), random(0, height), 10, 10);
+          }
+          
+          }
+        }
+    */
+
+
+
   }
 
-      //cara normal
-stroke(255,192,203);
-  fill(255,192,203);
+  //cara normal
+  stroke(255, 192, 203);
+  fill(255, 192, 203);
   ellipse(mouseX, mouseY, 35, 35);
-noStroke();
+  noStroke();
   //ojo s
-  fill(0,0,0);
-  ellipse(mouseX-8,mouseY-5,10,10);
-    ellipse(mouseX+8,mouseY-5,10,10);
+  fill(0, 0, 0);
+  ellipse(mouseX - 8, mouseY - 5, 10, 10);
+  ellipse(mouseX + 8, mouseY - 5, 10, 10);
   //boca
-  arc(mouseX,mouseY+5,20,20,TWO_PI,PI);
-  
-  
-    //PARA PONER LA CARA ROJA CUANDO SE ACERQUE AL SEGUNDERO
+  arc(mouseX, mouseY + 5, 20, 20, TWO_PI, PI);
+
+
+  //PARA PONER LA CARA ROJA CUANDO SE ACERQUE AL SEGUNDERO
   if (dist(mouseX, mouseY, seg2, seg2) < 40) {
-  //círculo que sigue al mouse
-  //cara preocupada
-  stroke(251,41,67);
-  fill(251,41,67);
-  ellipse(mouseX, mouseY, 35, 35);
-noStroke();
-  //ojo s
-  fill(0,0,0);
-  ellipse(mouseX-8,mouseY-5,10,10);
-    ellipse(mouseX+8,mouseY-5,10,10);
-  //boca
-  stroke(0,0,0);
-  strokeWeight(3);
-  line(mouseX-10,mouseY+10,mouseX+10,mouseY+10);
-  //cejas  
-  strokeWeight(4);
-  line(mouseX-14,mouseY-5,mouseX-6,mouseY-10);
-    line(mouseX+14,mouseY-5,mouseX+6,mouseY-10);
+    //círculo que sigue al mouse
+    //cara preocupada
+    stroke(251, 41, 67);
+    fill(251, 41, 67);
+    ellipse(mouseX, mouseY, 35, 35);
+    noStroke();
+    //ojo s
+    fill(0, 0, 0);
+    ellipse(mouseX - 8, mouseY - 5, 10, 10);
+    ellipse(mouseX + 8, mouseY - 5, 10, 10);
+    //boca
+    stroke(0, 0, 0);
+    strokeWeight(3);
+    line(mouseX - 10, mouseY + 10, mouseX + 10, mouseY + 10);
+    //cejas  
+    strokeWeight(4);
+    line(mouseX - 14, mouseY - 5, mouseX - 6, mouseY - 10);
+    line(mouseX + 14, mouseY - 5, mouseX + 6, mouseY - 10);
   }
-  
- //PARA PONER LA CARA ROJA CUANDO SE ACERQUE AL MINUTERO
-  if (dist(mouseX, mouseY, minu2, minu2) < 40) {
-  //cara preocupada
-  stroke(251,41,67);
-  fill(251,41,67);
-  ellipse(mouseX, mouseY, 35, 35);
-noStroke();
-  //ojo s
-  fill(0,0,0);
-  ellipse(mouseX-8,mouseY-5,10,10);
-    ellipse(mouseX+8,mouseY-5,10,10);
-  //boca
-  stroke(0,0,0);
-  strokeWeight(3);
-  line(mouseX-10,mouseY+10,mouseX+10,mouseY+10);
-  //cejas  
-  strokeWeight(4);
-  line(mouseX-14,mouseY-5,mouseX-6,mouseY-10);
-    line(mouseX+14,mouseY-5,mouseX+6,mouseY-10);
+
+  //PARA PONER LA CARA ROJA CUANDO SE ACERQUE AL MINUTERO
+  if (dist(mouseX, mouseY, minu2, minu2) < 30) {
+    //cara preocupada
+    stroke(251, 41, 67);
+    fill(251, 41, 67);
+    ellipse(mouseX, mouseY, 35, 35);
+    noStroke();
+    //ojo s
+    fill(0, 0, 0);
+    ellipse(mouseX - 8, mouseY - 5, 10, 10);
+    ellipse(mouseX + 8, mouseY - 5, 10, 10);
+    //boca
+    stroke(0, 0, 0);
+    strokeWeight(3);
+    line(mouseX - 10, mouseY + 10, mouseX + 10, mouseY + 10);
+    //cejas  
+    strokeWeight(4);
+    line(mouseX - 14, mouseY - 5, mouseX - 6, mouseY - 10);
+    line(mouseX + 14, mouseY - 5, mouseX + 6, mouseY - 10);
+    //AGREGA ELIPSES AZULEs
+    stroke(98, 152, 253);
+    fill(98, 152, 253);
+    strokeWeight(4);
+    ellipse(expMinuX, expMinuY, 20, 20);
+    expMinuX = random(0, width);
+    expMinuY = random(0, height);
+
+    for (var g = 0; g < 150; g++) {
+      fill(98, 152, 253);
+      ellipse(random(0, width), random(0, height), 20, 20);
+    }
   }
-  
-  
+
+
   //PARA PONER LA CARA ROJA CUANDO SE ACERQUE A LA HORA
-  if (dist(mouseX, mouseY, hora2, hora2) < 40) {
-  //círculo que sigue al mouse
-  //cara preocupada
-  stroke(251,41,67);
-  fill(251,41,67);
-  ellipse(mouseX, mouseY, 35, 35);
-noStroke();
-  //ojo s
-  fill(0,0,0);
-  ellipse(mouseX-8,mouseY-5,10,10);
-    ellipse(mouseX+8,mouseY-5,10,10);
-  //boca
-  stroke(0,0,0);
-  strokeWeight(3);
-  line(mouseX-10,mouseY+10,mouseX+10,mouseY+10);
-  //cejas  
-  strokeWeight(4);
-  line(mouseX-14,mouseY-5,mouseX-6,mouseY-10);
-    line(mouseX+14,mouseY-5,mouseX+6,mouseY-10);
+  if (dist(mouseX, mouseY, hora2, 320) < 40) {
+    //círculo que sigue al mouse
+    //cara preocupada
+    stroke(251, 41, 67);
+    fill(251, 41, 67);
+    ellipse(mouseX, mouseY, 35, 35);
+    noStroke();
+    //ojo s
+    fill(0, 0, 0);
+    ellipse(mouseX - 8, mouseY - 5, 10, 10);
+    ellipse(mouseX + 8, mouseY - 5, 10, 10);
+    //boca
+    stroke(0, 0, 0);
+    strokeWeight(3);
+    line(mouseX - 10, mouseY + 10, mouseX + 10, mouseY + 10);
+    //cejas  
+    strokeWeight(4);
+    line(mouseX - 14, mouseY - 5, mouseX - 6, mouseY - 10);
+    line(mouseX + 14, mouseY - 5, mouseX + 6, mouseY - 10);
+
+    //AGREGA ELIPSES BLANCAS
+    stroke(255, 255, 255);
+    strokeWeight(4);
+    ellipse(expHoraX, expHoraY, 10, 10);
+    expHoraX = random(0, width);
+    expHoraY = random(0, height);
+    for (var h = 0; h < 250; h++) {
+      fill(255, 255, 255);
+      ellipse(random(0, width), random(0, height), 35, 35);
+    }
   }
 
 }
