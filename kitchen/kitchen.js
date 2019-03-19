2efb16021c0cdb068e6b4b4db036abfead2779c9
sofiacastaneda <47247552+sofiacastaneda@users.noreply.github.com>
2019-03-19 06:56:09 -0500

    Add files via upload

--- kitchen/kitchen.js
+++ kitchen/kitchen.js
@@ -8,13 +8,24 @@ var y = 250;
 
 //paneo
 var hornear;
+var c=130;
+var d=25;
 //rate
 var batidora;
+var e=270;
+var f=20;
 //jump
 var cuchillo;
+var g=440;
+var h=70;
 
 var cancionj;
+var vol1;
 var cancionb;
+var vol2;
+
+var actual;
+
 
 
 function preload () {
@@ -29,33 +40,91 @@ function preload () {
 
   cancionj = loadSound('assets/clasica.mp3');
   cancionb = loadSound('assets/rock.mp3');
+  
 }
 
 function setup() {
   createCanvas(600, 400);
   background(100);
+ 
+  //actual = cancionb.currentTime();
+        //actual = cancionj.currentTime();
+
+
+//AJUSTAR EL VOLUMEN 0 ABAJO Y ALTO ARRIBA
+
+  //vol1 = map(mouseY, height, 0, 0.0, 1.1);
+  //cancionj.setVolume(vol1);
+//cancionj.play();
   
- //cancionj.setVolume(0.1);
- //cancionj.play();
-  
- //cancionb.setVolume(0.1);
- //cancionb.play();
+  //vol2 = map(y, height, 0, 0.0, 1.1);
+  //cancionb.setVolume(vol2);
+//cancionb.play();
+
   
 }
 
 function draw() {
 
+//PANEO PARA LA SECCIÓN QUE CUBRE LA IMAGEN DEL HORNO
+//hornear= map(mouseX, 100, 170, -1.0,1.0);
+//cancionj.pan(hornear);
+
+//hornear= map(x, 100, 170, -1.0,1.0);
+//cancionb.pan(hornear);
+
+//JAGGER
   noTint();
   image(fondo, 0, 0, width, height); 
   image(mick, mouseX, mouseY, 100, 100); 
   image(ludwig, x, y, 100, 100); 
 
 
-  image(hornear, 130, 25, 100, 70); 
-  image(batidora, 270, 20, 100, 110); 
-  image(cuchillo, 440, 70, 140, 50);
+//BEETHOVEN
+  image(hornear, c, d, 100, 70); 
+  image(batidora, e, f, 100, 110); 
+  image(cuchillo, g, h, 140, 50);
+  
+  //ignorar esto
+ // if (dist(MouseX, MouseY, c, d) < 20) {
+ // }
+   //ignorar esto
+
+
+
+
+//PARA QUE CUANDO SE ACERQUE A LA BATIDORA AUMENTE EL RATE
+  
+  //PERSONAJE JAGGER
+  // if (dist(MouseX, MouseY, e, f) < 20) {
+ // cancionj.rate(1+x);
+ //x = 0.5
+ // }
+ 
+   //PERSONAJE BEETHOVEN
+  // if (dist(x, y, e, f) < 20) {
+ // cancionb.rate(1+x);
+ //x = 0.5
+ // }
+ 
+ 
+ 
+ 
+ //PARA QUE CUANDO SE ACERQUE AL CUCHILLO SALTE
+
+  //PERSONAJE JAGGER
+  // if (dist(MouseX, MouseY, g, h) < 20) {
+ // cancionj.jump(actual + 1);
+ // }
+ 
+    //PERSONAJE BEETHOVEN
+ // if (dist(X, Y, g, h) < 20) {
+ // cancionB.jump(actual + 1);
+ // }
+  
 }
 
+//CONTROLES BEETHOVEN
 function keyPressed() {
 if (keyIsPressed == true) {
     if (keyCode == RIGHT_ARROW) {
@@ -79,13 +148,3 @@ if (keyIsPressed == true) {
 }
 
 }
-
-
-//if (dist(x, y, x1, y1) < 20) {
-//    x = 100;
-//    y = 100;
-//    x1 = 250;
-//    y1 = 100;
-//    puntajeA = 0;
-//    puntajeB = 0;
-//  }
